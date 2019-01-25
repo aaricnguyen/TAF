f94f9b56e27a622c4041e467989c23c565028c3a
tuan.changed <tuanchanged@gmail.com>
2019-01-25 10:12:56 +0700

    Add the @property to response object, init class demo for next week

--- TAF/Management/plugins/webservice/ResponseObject.py
+++ TAF/Management/plugins/webservice/ResponseObject.py
@@ -4,46 +4,59 @@ class ResponseObject():
     def __init__(self, response):
         self.response = response
 
+    @property
     def get_status_code(self):
         return self.response.status_code
-
+    
+    @property
     def get_elapsed_time(self):
         return self.response.elapsed
     
+    @property
     def get_ulr(self):
         return self.response.url
-
+    
+    @property
     def get_body_response_as_json(self):
         """Return a JSON Response Content"""
         return self.response.json()
-
+    
+    @property
     def get_body_response_as_text(self):
         """Return a Text Response Content"""
         return self.response.text
 
+    @property
     def get_body_response_as_bytes(self):
         """Return a Binary Response Content"""
         return self.response.content
     
+    @property
     def get_body_response_raw(self):
         """Return a Binary Response Content"""
         self.response.str   
         return self.response.raw
     
+    @property
     def get_response_body_size(self):
         pass
 
+    @property
     def get_headers(self):
         return self.response.headers
 
+    @property
     def get_content_type(self):
         return self.response.headers['Content-Type']
 
+    @property
     def get_response_header_size(self):
         pass
     
+    @property
     def is_json_content_type(self):
         pass
     
+    @property
     def is_text_content_type(self):
         pass
--- TAF/Management/plugins/webservice/WebService.py
+++ TAF/Management/plugins/webservice/WebService.py
@@ -31,104 +31,35 @@ class WebService(object):
             return ResponseObject(response)
 
     def verify_url(self, response_obj, expected_url):
-        assert response_obj.get_ulr() == expected_url, 'URL is mismatched as expected: {} instead of {}'.format(response_obj.get_ulr(), expected_url)
+        assert response_obj.get_ulr == expected_url, 'URL is mismatched as expected: {} instead of {}'.format(response_obj.get_ulr, expected_url)
         print ('URL is matched as expected: {}'.format(response_obj.get_ulr()))
 
     def verify_response_status_code(self, response_obj, expected_status_code):
-        assert response_obj.get_status_code() == expected_status_code, 'Status code is mismatched as expected: {} instead of {}'.format(expected_status_code, response_obj.get_status_code())
+        assert response_obj.get_status_code == expected_status_code, 'Status code is mismatched as expected: {} instead of {}'.format(expected_status_code, response_obj.get_status_code)
         print ('Status code is matched as expected: {}'.format(expected_status_code))
     
     def verify_response_status_code_in_range(self, response_obj, from_status_code, to_status_code):
-        assert response_obj.get_status_code() >= from_status_code and response_obj.get_status_code() <= to_status_code
+        assert response_obj.get_status_code >= from_status_code and response_obj.get_status_code <= to_status_code
         print ('Status code is matched as expected in the range: [{},{}]'.format(from_status_code, to_status_code))
     
     def verify_content_type(self, response_obj, expected_content_type):
-        assert response_obj.get_content_type() == expected_content_type, 'Content-Type is mismatched as expected: {} instead of {}'.format(expected_content_type, response_obj.get_content_type())
+        assert response_obj.get_content_type == expected_content_type, 'Content-Type is mismatched as expected: {} instead of {}'.format(expected_content_type, response_obj.get_content_type)
         print ('Content-Type is matched as expected: {}'.format(expected_content_type))
   
-    def verify_header_field(self):
+    def verify_header_fields(self):
         pass
     
     def verify_body_response(self, response_obj, body_response):
         pass
     
-    def contains_string(self, response_obj, string):
+    def verify_body_response_has_contains_string(self, response_obj, string):
         pass
     
-    def verify_something_more(self):
+    def verify_body_response_equal_to_a_string(self, response_obj, string):
         pass
-
-def main():
-    demo_status_code()
-    demo_headers()
-    demo_body_response()
-
-def demo_status_code():
-    # Create a Request Object
-    req = RequestObject()
-    req.set_url('https://httpbin.org/post')
-    req.set_method('Post')
-    req.set_data({'key1':'value1'})
-    # Create a Web Service allow sent request
-    ws = WebService("Demo")
-    res = ws.send_request(req)
-    print ('--- Get URL')
-    print (res.get_ulr())
-    print ('--- Get Status Code')
-    print (res.get_status_code())
-
-def demo_headers():
-    # Create a Request Object
-    req = RequestObject()
-    req.set_url('https://httpbin.org/post')
-    req.set_method('Post')
-    req.set_data({'key1':'value1'})
-    # Create a Web Service allow sent request
-    ws = WebService("Demo")
-    res = ws.send_request(req)
-    print ('--- Get Headers')
-    print (res.get_headers())
-
-def demo_body_response():
-    # Create a Request Object
-    req = RequestObject()
-    req.set_url('https://httpbin.org/post')
-    req.set_method('Post')
-    req.set_data({'key1':'value1'})
-    # Create a Web Service allow sent request
-    ws = WebService("Demo")
-    res = ws.send_request(req)
-    print ('--- Get Body Content As Json')
-    print (res.get_body_response_as_json())
-    print ('--- Get Body Content As Text')
-    print (res.get_body_response_as_text())
-    print ('--- Get Body Content As Bytes')
-    print (res.get_body_response_as_bytes())
-
-def demo_verify():
-    print('Start Demo 2')
-
-    WS = WebService('My Web Service')
     
-    request = RequestObject()
-    request.set_name('Get The GitHub\'s public timeline')
-    request.set_url('https://api.github.com/events')
-    request.set_method('GET')
+    def verify_response_time_less_than(self, response_obj, expected_time_in_miliseconds):
+        pass
     
-    response = WS.send_request(request)
-    WS.verify_response_status_code(response, 200)
-    WS.verify_content_type(response, "application/json; charset=utf-8")
-
-if __name__ == "__main__":
-    main()
-
-#     print(response_object.text)
-#     print(response_object.json())
-#     print(response_object.content)
-#     print(response_object.content.decode("utf-8"))
-#     # Headers is a dictionary
-#     print(response_object.headers)
-#     
-#     # Get the content-type from the dictionary.
-#     print(response_object.headers["content-type"])
-    
+    def verify_response_time_in_range(self, response_obj, from_time_in_miliseconds, to_time_in_miliseconds):
+        pass
